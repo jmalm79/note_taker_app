a53d5886b67a89899c45f0ec82be25e192c7c4e4
Josh A <jmalm79@gmail.com>
2021-04-09 00:05:14 -0500

    updates to server.js file, have working notes

---
+++ server.js
@@ -0,0 +1,45 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+const fs = require('fs');
+
+//Express
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+//Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+});
+
+app.get('/api/notes/', (req, res) => {
+    var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    console.log(data);
+    res.json(data);
+});
+
+app.post('/api/notes', (req, res) => {
+    var allNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    var newNote = req.body;
+    var id = allNotes.length
+
+    newNote.id = id + 1;
+    allNotes.push(newNote);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(allNotes));
+    console.log("Note Saved");
+    res.json(allNotes);
+
+});
+
+
+
+// Listener
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
